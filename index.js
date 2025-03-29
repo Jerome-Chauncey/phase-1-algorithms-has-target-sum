ee5ac2bac15e08b2163215834eb6e9d28ac49b92
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-03-29 13:19:25 +0300

    impements big o notation, use of pseudocode and iteration

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-function hasTargetSum(array, target) {
+
   // Write your algorithm here
-}
+  function hasTargetSum (array, target){
+    for (let i = 0; i < array.length; i++){
+      for (let j = i + 1; j < array.length; j++){
+        if (array[i] + array[j] === target){
+          return true;
+        }
+      }
+    }
+    return false;
+    }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-*/
 
 /*
   Add written explanation of your solution here
+    so we should have two numbers from the array adding up to the target and return true, if no return false
+    first we write a function called hasTargetSum that takes two arguments (array and target)
+    we loop through the array and pick the first number in the pair(i = index)
+    we loop through the array and pick the second number in the pair(j = index) j = i + 1 to avoid repeats and not to compare the same number twice
+    we check if the pair sums to target
+    return true if pair found 
+    return false of pair not found
+
+
+  // Write the Big O time complexity of your function here
+  // O(n²) slow (one by one)
+
+  //space complexity
+  //0(1) fastest(accessing array index)
+
+
+/* 
+  Add your pseudocode here
+  initailize an array hasTargetSum 
+  iterate through each index & compare every possible pair of numbers in the array
+  two itterations i & j
+  check if their sum is equal to target
+  return true if equal
+  return false if not
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
